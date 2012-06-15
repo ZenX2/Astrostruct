64b47dd8f636ee53211a9a2abfeb6048ddd07d33
naelstrof <naelstrof@gmail.com>
2012-06-15 00:39:13 -0700

    oops left garbage

--- windowsbin/data/textures/lights/point.lua
+++
@@ -1,3 +0,0 @@
-Static = Animation("textures/lights/point.png")
-Static.Name = "idle"
-LoadTexture("point",Static)
--- windowsbin/data/textures/maps/grimy.lua
+++
@@ -1,4 +0,0 @@
-Grimy = Animation("textures/maps/grimy.png")
-Grimy.TileSize = 64
-
-LoadTexture("grimy",Grimy);
